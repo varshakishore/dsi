b3fcbf8ece6b47ddbd9fb0a33930da6cbe7f7b8f
justinlovelace <jl3353@cornell.edu>
2023-01-19 00:39:56 -0500

    Optimization for new splits

--- dsi_model_v1.py
+++ dsi_model_v1.py
@@ -306,12 +306,15 @@ def validate_script(args, tokenizer, model, doc_type=None, split=None):
 
     if doc_type == "old":
         data_dir = os.path.join(args.base_data_dir, 'old_docs')
+        doc_class = joblib.load(os.path.join(data_dir, 'doc_class.pkl'))
     elif doc_type == "new":
         data_dir = os.path.join(args.base_data_dir, 'new_docs')
+        doc_class = joblib.load(os.path.join(data_dir, 'doc_class.pkl'))
     elif doc_type == "tune":
         data_dir = os.path.join(args.base_data_dir, 'tune_docs')
-
-    doc_class = joblib.load(os.path.join(data_dir, 'doc_class.pkl'))
+        doc_class = joblib.load('/home/jl3353/dsi/data/NQ320k/tune_docs/doc_class.pkl')
+    else:
+        raise ValueError(f'doc_type={doc_type} must be old, new, or tune')
 
     if split == "train":
 
@@ -360,11 +363,13 @@ def validate_script(args, tokenizer, model, doc_type=None, split=None):
         data_files=os.path.join(data_dir, 'passages_unseen.json'),
         ignore_verifications=False,
         cache_dir='cache'
-    )['train']
+        )['train']
 
         print('unseen generated queries loaded')
+    else:
+        raise ValueError(f'split={split} must be train, valid, test, seenq, or unseenq')
 
-    if split == "train" or split == "val":
+    if split == "train" or split == "valid":
         dataset =  DSIqgTrainDataset(tokenizer=tokenizer, datadict = data, doc_class = doc_class)
 
     elif split == "seenq" or split == "unseenq":
